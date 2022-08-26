236157433868e6eeb01ceadb62789354c64df415
vitaliy197811 <vital197811@gmail.com>
2022-08-26 18:48:46 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const liElem = document.querySelector('#ingredients')
+
+const element = ingredients.map(optional => {
+  const liElement = document.createElement('li');
+  liElement.textContent = optional;
+  liElement.classList.add('item');
+  return liElement;
+})
+liElem.append(...element);
+console.log(element);

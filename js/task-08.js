b04a1039532889418f5ac5c7b42aa5d0af67583e
vitaliy197811 <vital197811@gmail.com>
2022-08-30 19:53:45 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector( ".login-form");
 
+form.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    if (email !== "" && password !== "") {
+        const formData = {
+            email,
+            password,
+        };
+        console.log(formData);
+    } else {
+        alert("Все поля должны быть заполнены!!!");
+    }
+    return form.reset();
+}
+// 1@mail.com

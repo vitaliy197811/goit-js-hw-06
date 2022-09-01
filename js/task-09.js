b547ed390d781ecc2d4c042f190789ec956e1761
vitaliy197811 <vital197811@gmail.com>
2022-09-01 18:43:26 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const color = document.querySelector( ".color");
+const buttonСhangeСolor = document.querySelector( ".change-color");
+console.log(color);
+
+buttonСhangeСolor.addEventListener("click", changeСolor);
+
+function changeСolor (even) {
+  const changeColor = getRandomHexColor();
+  document.body.style.backgroundColor = changeColor;
+  color.textContent = changeColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

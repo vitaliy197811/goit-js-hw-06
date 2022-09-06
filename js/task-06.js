772ef475123669e34998a4c201cd9c5a4d2c0ff5
Vitaliy <103895059+vitaliy197811@users.noreply.github.com>
2022-09-06 20:41:22 +0300

    task6-01

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,8 @@ const input = document.querySelector( "#validation-input");
 
 input.addEventListener(`blur`, () => {
     input.classList.add("invalid");
-    if (input.value.length === 6) {
+    const inputLength = Number(input.dataset.length);
+    if (input.value.length === inputLength) {
         input.classList.toggle("invalid");
         input.classList.add("valid");
     } 
